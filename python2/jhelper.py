84eaa6033af7b7b40c6c39038ab76d0863c370f4
slasheks <slasheksu@gmail.com>
2016-06-14 00:03:45 +0000

    testnet mainnet options

--- python2/jhelper.py
+++ python2/jhelper.py
@@ -27,6 +27,10 @@ parser.add_argument('-s1', '--site1', dest='site1', action='store',
 parser.add_argument('-s2', '--site2', dest='site2', action='store',
                     default='https://lisk-login.vipertkd.com',help='Site2')
 
+parser.add_argument('-u', '--url', dest='url', action='store',
+                    default='http://localhost:7000',
+                    help='Url in format: http://localhost:7000')
+
 parser.add_argument('-f', '--file', dest='infile', action='store',
                     help='Input File')
 
@@ -72,7 +76,7 @@ except KeyboardInterrupt:
 
 
 payl = {'secret':passphrase}
-api = liskAPI.liskAPI('http://127.0.0.1:8000')
+api = liskAPI.liskAPI(args.url)
 
 account = api.account('open_account', payl)
 
@@ -81,34 +85,34 @@ if int(account['account']['balance']) < 1:
     print "No lisk in that account"
     sys.exit(1)
 
-sender = liskAPI.liskAPI('http://127.0.0.1:8000')
+sender = liskAPI.liskAPI(args.url)
 verify1 = liskAPI.liskAPI(args.site1)
 verify2 = liskAPI.liskAPI(args.site2)
 
 try:
 
     with open(args.infile) as jfhi:
-    
+
         jcsvr = csv.reader(jfhi)
-    
+
         # Loop through the csv file and send transactions
         for row in jcsvr:
-    
+
             # [0] address # [1] amount # [3] txid
-    
+
             address = row[0]
             amount = row[1]
-    
+
             amountstr = str(amount)
-    
+
             strsplit = amountstr.split('.')
-    
+
             if len(strsplit[1]) > 8:
-    
+
                 print "Invalid decimal places. Ex: 1.12345678"
-    
+
                 exit(1)
-    
+
             amnt = float(amount) * math.pow(10, 8) # amount times ten to the power of eigth
 
             sendpayload = {
@@ -116,7 +120,7 @@ try:
                 'recipientId' : address,
                 'amount' : int(math.ceil(amnt))
             }
-    
+
             try:
 
                 response = sender.transactions('send', sendpayload)
@@ -127,13 +131,13 @@ try:
 
                 print "Error {}".format(e)
                 sys.exit(1)
-    
+
             if response['success'] == False:
 
                 print response
                 logging.info(response)
                 sys.exit(1)
-                
+
 
             logging.info(response)
 
@@ -156,24 +160,24 @@ try:
 
                     break
 
-    
+
             time.sleep(20)
 
             if response['success']:
-    
+
                 row.append(tx)
-    
+
                 while True:
 
                     confirmation1 = verify1.transactions('get_tx', payload)
                     confirmation1u = verify1.transactions('unconfirmed', payload)
-    
+
                     confirmation2 = verify2.transactions('get_tx', payload)
                     confirmation2u = verify2.transactions('unconfirmed', payload)
-    
+
                     if (confirmation1['success'] or confirmation1u['success'])\
                         and (confirmation2['success'] or confirmation2u['success']):
-    
+
                         print "Confirmed transaction {} is on 2 remote nodes".format(tx)
 
                         with open('successout.csv', 'a') as jfho:
@@ -186,19 +190,19 @@ try:
                             jcsvw.writerow(row)
 
                         break
-    
+
                     else:
-    
+
                         continue
-    
+
 
             else:
-    
+
                 print "Transaction Failure"
-    
+
                 sys.exit(1)
 
-except Exception as e:    
+except Exception as e:
 
     raise
     print "Exiting... {}".format(e)
